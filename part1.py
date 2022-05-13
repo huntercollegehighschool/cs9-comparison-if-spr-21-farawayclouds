cbe32f1dc2faa0ee6ce6648d33b0728710401bd0
Fara Hameed <103280972+farawayclouds@users.noreply.github.com>
2022-05-13 18:00:49 +0000

    finish p1

--- part1.py
+++ part1.py
@@ -19,9 +19,12 @@ number = int(input("Enter a number: "))
 
 smallest = number
 
-number = int(input("Enter another number: "))
+poo = int(input("Enter a second number: "))
 
-if number < smallest:
-  smallest = number
-  
+next = int(input("Enter a third number: "))
+
+if poo < smallest :
+  smallest = poo
+elif next < smallest :
+  smallest = next
 print("The smallest number is ", smallest)
